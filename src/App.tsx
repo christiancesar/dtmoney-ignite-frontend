09cb4924a94e62906a2f2d7dbbc5a0e958825d4e
christiancesar <christiancnp@gmail.com>
2021-06-13 22:33:53 -0400

    create components newTransactionModal, TransactionTable, include MirageJs and React-Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useCallback, useState } from "react";
+
+
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true)
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false)
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Dashboard />      
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
